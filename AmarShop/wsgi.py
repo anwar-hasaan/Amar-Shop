9df5244bd95eeae87c4b3d4e59a2ce7e428f278f
anwar-hasaan <anwarhossene@gmail.com>
2022-12-22 22:29:42 +0600

    nothing much

--- AmarShop/wsgi.py
+++ AmarShop/wsgi.py
@@ -9,9 +9,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-import dotenv
-
-dotenv.read_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AmarShop.settings')
 
